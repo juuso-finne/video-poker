74ac27090952340230a951fb7173396842f3fd0d
juuso-finne <juuso.f@gmail.com>
2026-04-08 06:02:33 +0300

    Add Button subclasses

--- src/Button/button.cpp
+++ src/Button/button.cpp
@@ -1,8 +1,33 @@
 #include "button.h"
 
-Button::Button(const Rectangle &bounds, const char *text){
-    bounds_ = bounds;
-    text_ = text;
-    enabled_ = false;
-    clicked_ = false;
+Button::Button(const Vector2 &position){
+    bounds_ = Rectangle{position.x, position.y, kButtonWidth, kButtonHeight};
+}
+
+DealButton::DealButton(const Vector2 &position): Button(position){
+    type_ = ButtonType::kDeal;
+}
+
+BetButton::BetButton(const Vector2 &position): Button(position){
+    type_ = ButtonType::kBet;
+}
+
+HoldButton::HoldButton(const Vector2 &position): Button(position){
+    type_ = ButtonType::kHold;
+}
+
+CashOutButton::CashOutButton(const Vector2 &position): Button(position){
+    type_ = ButtonType::kCashOut;
+}
+
+DoubleButton::DoubleButton(const Vector2 &position): Button(position){
+    type_ = ButtonType::kDouble;
+}
+
+SmallButton::SmallButton(const Vector2 &position): Button(position){
+    type_ = ButtonType::kSmall;
+}
+
+BigButton::BigButton(const Vector2 &position): Button(position){
+    type_ = ButtonType::kBig;
 }
--- src/Button/button.h
+++ src/Button/button.h
@@ -2,12 +2,59 @@
 #include<raylib.h>
 #include <string>
 
+constexpr int kButtonWidth = 100;
+constexpr int kButtonHeight = 50;
+
+enum class ButtonType{
+    kDeal,
+    kBet,
+    kHold,
+    kCashOut,
+    kDouble,
+    kSmall,
+    kBig
+};
+
 class Button{
     public:
-        Button(const Rectangle &bounds, const char *text);
-        bool enabled_;
-        bool clicked_;
+        Button(const Vector2 &position);
+        ButtonType type_;
         Rectangle bounds_;
         const char *text_;
     private:
+};
+
+class DealButton: public Button{
+    public:
+        DealButton(const Vector2 &position);
+};
+
+class BetButton: public Button{
+    public:
+        BetButton(const Vector2 &position);
+};
+
+class HoldButton: public Button{
+    public:
+        HoldButton(const Vector2 &position);
+};
+
+class CashOutButton: public Button{
+    public:
+        CashOutButton(const Vector2 &position);
+};
+
+class DoubleButton: public Button{
+    public:
+        DoubleButton(const Vector2 &position);
+};
+
+class SmallButton: public Button{
+    public:
+        SmallButton(const Vector2 &position);
+};
+
+class BigButton: public Button{
+    public:
+        BigButton(const Vector2 &position);
 };
