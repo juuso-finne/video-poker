55410d5f97c9987b88ed0573c3260bbf3f159bf7
juuso-finne <juuso.f@gmail.com>
2026-04-13 01:09:31 +0300

    Draw state: Added console info. Reset deck after hand is evaluated

--- src/Game/GameState/States/drawState.cpp
+++ src/Game/GameState/States/drawState.cpp
@@ -30,17 +30,20 @@ void DrawState::Deal(){
         }
     }
     GameData::player_hand_ = new_hand;
+    GameData::deck_.Reset();
 
     HandValue value = EvaluateHand(GameData::player_hand_);
     int coefficient = payout_table[value];
 
     if(coefficient == 0){
-        std::cout << "No win!" << std::endl;
+        std::cout
+            << "No win! Total winnings: " << GameData::total_winnings_ << std::endl
+            << "Total bets: " << GameData::total_bets_ << std::endl
+            << "Net profit: " << GameData::total_winnings_ - GameData::total_bets_ << std::endl;
         ChangeState(InitialState::Instance());
     } else{
         GameData::current_winnings_ = coefficient * GameData::bet_;
         std::cout << "You won " << GameData::current_winnings_ << "! Do you want to double?" << std::endl;
-        GameData::deck_.Reset();
         ChangeState(WinState::Instance());
     }
 }
