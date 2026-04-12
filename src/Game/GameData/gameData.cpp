e584f3e4c853317a7e09168c14700a0d52248120
juuso-finne <juuso.f@gmail.com>
2026-04-12 03:52:18 +0300

    Add variables to GameData

--- src/Game/GameData/gameData.cpp
+++ src/Game/GameData/gameData.cpp
@@ -1,5 +1,12 @@
 #include "gameData.h"
 #include "../GameState/gameState.h"
 
-int GameData::bet_ = 1;
+int GameData::bet_ = 20;
+int GameData::current_winnings_ = 0;
+int GameData::total_winnings_ = 0;
+int GameData::total_bets_ = 0;
+
+bool GameData::held_cards_[5];
+
+std::vector<Card> GameData::player_hand_;
 GameState* GameData::state_ = InitialState::Instance();
--- src/Game/GameData/gameData.h
+++ src/Game/GameData/gameData.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <memory>
+#include "../../Card/card.h"
+#include<vector>
 
 class GameState;
 
@@ -7,5 +8,12 @@ class GameState;
 class GameData{
     public:
         static GameState *state_;
+
         static int bet_;
+        static int current_winnings_;
+        static int total_winnings_;
+        static int total_bets_;
+
+        static std::vector<Card> player_hand_;
+        static bool held_cards_[5];
 };
