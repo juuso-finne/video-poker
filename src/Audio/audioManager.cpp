3d3216466922296005fa41b01e41204f1d2d2795
juuso-finne <juuso.f@gmail.com>
2026-04-22 15:51:10 +0300

    Add deal sound

--- src/Audio/audioManager.cpp
+++ src/Audio/audioManager.cpp
@@ -2,6 +2,7 @@
 
 Sound AudioManager::coin_sound_ = Sound{};
 Sound AudioManager::shuffle_sound_ = Sound{};
+Sound AudioManager::deal_sound_ = Sound{};
 
 void LoadSounds(){
     InitAudioDevice();
@@ -10,6 +11,7 @@ void LoadSounds(){
     SetSoundVolume(AudioManager::coin_sound_, 0.15);
 
     AudioManager::shuffle_sound_ = LoadSound("assets/sounds/shuffle.mp3");
+    AudioManager::deal_sound_ = LoadSound("assets/sounds/deal.mp3");
 
 
 }
@@ -17,5 +19,6 @@ void LoadSounds(){
 void UnloadSounds(){
     UnloadSound(AudioManager::coin_sound_);
     UnloadSound(AudioManager::shuffle_sound_);
+    UnloadSound(AudioManager::deal_sound_);
     CloseAudioDevice();
 }
--- src/Audio/audioManager.h
+++ src/Audio/audioManager.h
@@ -6,6 +6,7 @@ class AudioManager{
     public:
         static Sound coin_sound_;
         static Sound shuffle_sound_;
+        static Sound deal_sound_;
 };
 
 void LoadSounds();
