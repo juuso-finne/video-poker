f70daae985b916bd66410ef0123920334574ba66
juuso-finne <juuso.f@gmail.com>
2026-04-23 13:38:30 +0300

    Clean up main method

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,25 @@
 #include <raylib.h>
-#include "Card/deck.h"
 #include "Gui/gui.h"
 #include "Game/GameData/gameData.h"
 #include "Game/GameState/gameState.h"
-#include "Game/GameRules/gameRules.h"
 #include "Audio/audioManager.h"
-#include <memory>
-#include <iostream>
-
 
 int main()
 {
 
-/*     float delay = 0.25;
-    std::vector<Card> cards{}; */
-
     InitWindow(ScreenConstants::screen_width_, ScreenConstants::screen_height_, "Video poker");
     Texture2D card_sprite_sheet = LoadTexture("assets/graphics/card_spritesheet.png");
+
     LoadSounds();
-
-
-
-
-
     SetTargetFPS(60);
-
 
     while (!WindowShouldClose())
     {
-
-/*         delay -= GetFrameTime();
-        if(delay < 0 ){
-            delay = 0.5;
-            if(deck.IsEmpty()){
-                delay = 1;
-                deck.Reset();
-            }
-            Card c = deck.DealOne();
-            c.FaceUp();
-            c.Move({775, 300}, GetFrameTime() * 250);
-            cards.push_back(c);
-        } */
-
         UpdateMusic();
+
         BeginDrawing();
         ClearBackground(ScreenConstants::background_color_);
 
-        //Gui::PlayAnimations();
         Gui::Update(card_sprite_sheet);
 
         if(Gui::animations_.empty() && GameState::state_changed_){
@@ -54,18 +27,6 @@ int main()
             GameData::state_ -> Init();
         }
 
-/*         for (std::vector<Card>::iterator it = cards.begin(); it != cards.end();)
-        {
-            if (!(*it).IsMoving()){
-                it = cards.erase(it);
-            }
-            else{
-                (*it).UpdateAndDraw();
-                ++it;
-            }
-        }
-
- */
         EndDrawing();
     }
 
